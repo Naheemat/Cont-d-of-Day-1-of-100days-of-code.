97be8b45f1c2c12fc47e3fcf6da2af8541e912e7
Naheemat <114004046+Naheemat@users.noreply.github.com>
2023-07-03 23:42:32 -0700

    Add files via upload

---
+++ Band_Name Generator.py
@@ -0,0 +1,9 @@
+#create a greeting.
+print('Hi!, Welcome to my Band Name Generator.')
+#Ask the user for the city that they grew up
+User_City = input('Please, Enter the city where you grew up \n')
+#Ask the user for the name of a pet
+User_petname = input('What is the name of our favorite pet? \n')
+#Combinine user's name and pet name
+Band_Name = ('Your Band Name could be ' + User_City + ' ' + User_petname)
+print(Band_Name)
